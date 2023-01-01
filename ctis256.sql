c9c2b74cbd9aefe5c3c2b2774c60e51ad68deb2c
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2023-01-01 04:45:14 +0300

    Update db schema

--- ctis256.sql
+++ ctis256.sql
@@ -3,59 +3,45 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 01, 2023 at 12:22 AM
+-- Generation Time: Jan 01, 2023 at 01:43 AM
 -- Server version: 5.7.34
--- PHP Version: 7.4.21
-
+-- PHP Version: 8.0.8
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
 START TRANSACTION;
+
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `ctis256`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
-
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` text NOT NULL,
-  `email` text NOT NULL,
-  `password` text NOT NULL,
-  `userclass` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `id` int(11) NOT NULL,
+    `username` text NOT NULL,
+    `email` text NOT NULL,
+    `password` text NOT NULL,
+    `userclass` varchar(40) NOT NULL DEFAULT 'student') ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 --
 -- Indexes for dumped tables
 --
-
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
 --
 -- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
