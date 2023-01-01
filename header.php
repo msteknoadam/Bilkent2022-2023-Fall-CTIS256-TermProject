f8168204cfc984104fef8f2509ecb4ad5e31a459
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2023-01-01 05:30:14 +0300

    Add jQuery to all pages

--- header.php
+++ header.php
@@ -3,4 +3,5 @@
 
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        
+
+<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
