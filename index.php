952ed2c281003512b62340ebe723a8783f1c8827
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2023-01-01 04:42:32 +0300

    Initialize index page

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+include_once "db.php";
+session_start();
+
+if (isset($_POST["logout"])) {
+    $_SESSION = [];
+    session_abort();
+    setcookie(session_name(), "", 1, "/");
+}
+
+if (isset($_SESSION["user"]) && isset($_SESSION["user"]["id"])) {
+    $user_stmt = $db->prepare("select * from users where id = ?");
+    $user_stmt->execute([$_SESSION["user"]["id"]]);
+    $user = $user_stmt->fetch();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1><a href="login.php">Login</a></h1>
+    <h1><a href="register.php">Register</a></h1>
+
+   <?php if (isset($user)): ?>
+    <form action="" method="post">
+        <input type="submit" value="Logout" name="logout">
+    </form>
+    <?php endif;?>
+
+    <?php
+if (isset($user)) {
+    var_dump($user);
+}
+?>
+</body>
+</html>
