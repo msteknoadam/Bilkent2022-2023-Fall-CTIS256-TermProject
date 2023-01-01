74fbf296f39bafeaca8a1b55db5bce4e2ce87f79
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2023-01-01 03:31:07 +0300

    Better error page

--- error.php
+++ error.php
@@ -5,8 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Error!</title>
+    <style>
+        #back {
+            text-decoration: underline;
+            color: blue;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
     <h2>Sorry, but something went wrong while fetching data from database. Please try again later!</h2>
+    <br><br>
+    <h3>Click <a id="back" onclick="history.back()">here</a> to go back to your previous page.</h3>
 </body>
 </html>
