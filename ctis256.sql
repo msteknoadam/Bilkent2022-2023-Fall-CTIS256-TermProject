f5dec4fd4e53c7f79c081988baee1f9b917aeba6
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2023-01-01 20:12:16 +0300

    Update db schema

--- ctis256.sql
+++ ctis256.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 01, 2023 at 11:52 AM
+-- Generation Time: Jan 01, 2023 at 05:12 PM
 -- Server version: 5.7.34
 -- PHP Version: 8.0.8
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -36,13 +36,14 @@ CREATE TABLE `projects` (
     `state` text NOT NULL,
     `required_software` text NOT NULL,
     `required_hardware` text NOT NULL,
-    `members` text NOT NULL) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+    `members` text NOT NULL,
+    `owner_uid` int(11) NOT NULL) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 --
 -- Dumping data for table `projects`
 --
-INSERT INTO `projects` (`id`, `name`, `description`, `year`, `semester`, `requirements`, `state`, `required_software`, `required_hardware`, `members`)
-    VALUES (1, 'CTIS256 Term Project', 'Term Project of Backend Development Course', '2022-2023', 'Fall', 'Make different userclasses\r\nGive each different permission', 'waiting', 'PHP\r\nMySQL\r\nApache', 'Computer\r\nInternet (actually not a hardware)', 'Ayse\r\nGoktug'), (2, 'CTIS257 Term Project', 'Term Project of CTIS257', '2022-2023', 'Spring', 'Page should load \"fast\" hehe\r\n99.999% Uptime', 'rejected', 'NodeJS\r\nNginx\r\nPostgreSQL', 'Computer\r\nInternet', 'Ayse\r\nGoktug'), (3, 'CTIS255 Term Project', 'Term Project of CTIS255 Lecture', '2021-2022', 'Spring', 'At least 5 different pages\r\nOne of the pages have to include game', 'accepted', 'HTML\r\nCSS\r\nJS\r\njQuery', 'Computer\r\nInternet', 'Ayse\r\nGoktug');
+INSERT INTO `projects` (`id`, `name`, `description`, `year`, `semester`, `requirements`, `state`, `required_software`, `required_hardware`, `members`, `owner_uid`)
+    VALUES (3, 'CTIS255 Term Project', 'Term Project of CTIS255 Lecture', '2021-2022', 'Spring', 'At least 5 different pages\r\nOne of the pages have to include game', 'accepted', 'HTML\r\nCSS\r\nJS\r\njQuery', 'Computer\r\nInternet', 'Ayse\r\nGoktug', 0);
 
 -- --------------------------------------------------------
 --
@@ -66,7 +67,7 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `userclass`, `name`, `firmname`, `city`, `district`, `address`)
-    VALUES (1, 'msteknoadam', 'yesilyurt.gok@gmail.com', 'test', 'student', NULL, NULL, NULL, NULL, NULL), (2, 'msteknoadama', 'yesilyurt.gok@gmail.coma', 'aa', 'student', NULL, NULL, NULL, NULL, NULL), (3, 'msteknoadamaa', 'yesilyurt.gok@gmail.comaa', 'aaa', 'student', NULL, NULL, NULL, NULL, NULL), (4, 'msteknoadamz', 'yesilyurt.gok@gmail.comz', 'z', 'student', NULL, NULL, NULL, NULL, NULL), (5, 'msteknoadamzz', 'yesilyurt.gok@gmail.comzz', 'azz', 'student', NULL, NULL, NULL, NULL, NULL), (6, 'msteknoadamas', 'yesilyurt.gok@gmail.comas', 'as', 'student', NULL, NULL, NULL, NULL, NULL), (7, 'msteknoadamaaaa', 'yesilyurt.gok@gmail.comaaa', 'aaaa', 'student', NULL, NULL, NULL, NULL, NULL), (8, 'msteknoadamaz', 'yesilyurt.gok@gmail.comaz', 'az', 'student', NULL, NULL, NULL, NULL, NULL), (9, 'msteknoadamqq', 'yesilyurt.gok@gmail.comqq', 'qq', 'instructor', NULL, NULL, NULL, NULL, NULL), (10, 'msteknoadamstu1', 'yesilyurt.gok@gmail.comstu1', 'stu1', 'student', 'student5', NULL, NULL, NULL, NULL), (11, 'msteknoadamfirm1', 'yesilyurt.gok@gmail.comfirm1', 'firm1', 'firm', NULL, NULL, NULL, NULL, NULL), (12, 'msteknoadamfirm2', 'yesilyurt.gok@gmail.comfirm2', 'firm2', 'firm', '', 'firm2', 'city', 'district', 'address'), (13, 'msteknoadamfirm3', 'yesilyurt.gok@gmail.comfirm3', 'firm3', 'firm', '', 'firm3', 'city', 'district', 'address'), (14, 'msteknoadaminst1', 'yesilyurt.gok@gmail.cominst1', 'inst1', 'instructor', 'inst1', '', '', '', ''), (15, 'msteknoadamfirm5', 'yesilyurt.gok@gmail.comfirm5', 'firm5', 'firm', '', 'firm5', 'city', 'district', 'address');
+    VALUES (1, 'msteknoadam', 'yesilyurt.gok@gmail.com', 'test', 'student', NULL, NULL, NULL, NULL, NULL), (2, 'msteknoadama', 'yesilyurt.gok@gmail.coma', 'aa', 'student', NULL, NULL, NULL, NULL, NULL), (3, 'msteknoadamaa', 'yesilyurt.gok@gmail.comaa', 'aaa', 'student', NULL, NULL, NULL, NULL, NULL), (4, 'msteknoadamz', 'yesilyurt.gok@gmail.comz', 'z', 'student', NULL, NULL, NULL, NULL, NULL), (5, 'msteknoadamzz', 'yesilyurt.gok@gmail.comzz', 'azz', 'student', NULL, NULL, NULL, NULL, NULL), (6, 'msteknoadamas', 'yesilyurt.gok@gmail.comas', 'as', 'student', NULL, NULL, NULL, NULL, NULL), (7, 'msteknoadamaaaa', 'yesilyurt.gok@gmail.comaaa', 'aaaa', 'student', NULL, NULL, NULL, NULL, NULL), (8, 'msteknoadamaz', 'yesilyurt.gok@gmail.comaz', 'az', 'student', NULL, NULL, NULL, NULL, NULL), (9, 'msteknoadamqq', 'yesilyurt.gok@gmail.comqq', 'qq', 'instructor', NULL, NULL, NULL, NULL, NULL), (10, 'msteknoadamstu1', 'yesilyurt.gok@gmail.comstu1', 'stu1', 'student', 'student5', NULL, NULL, NULL, NULL), (11, 'msteknoadamfirm1', 'yesilyurt.gok@gmail.comfirm1', 'firm1', 'firm', NULL, NULL, NULL, NULL, NULL), (12, 'msteknoadamfirm2', 'yesilyurt.gok@gmail.comfirm2', 'firm2', 'firm', '', 'firm2', 'city', 'district', 'address'), (13, 'msteknoadamfirm3', 'yesilyurt.gok@gmail.comfirm3', 'firm3', 'firm', '', 'firm3', 'city', 'district', 'address'), (14, 'msteknoadaminst1', 'yesilyurt.gok@gmail.cominst1', 'inst1', 'instructor', 'inst1', '', '', '', ''), (15, 'msteknoadamfirm5', 'yesilyurt.gok@gmail.comfirm5', 'firm5', 'firm', '', 'firm5', 'city', 'district', 'address'), (16, 'msteknoadamadmin1', 'yesilyurt.gok@gmail.comadmin1', 'admin1', 'admin', 'admin1', '', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -94,7 +95,7 @@ ALTER TABLE `projects` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREME
 --
 -- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 16;
+ALTER TABLE `users` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 17;
 
 COMMIT;
 
